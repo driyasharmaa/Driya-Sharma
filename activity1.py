742d628da611008d6ceb119c8e8679524e7ad7e8
driyasharmaa <sharmadriya15@gmail.com>
2025-03-19 18:43:48 +0000

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,2 +1,13 @@
-print ("Welcome to the world of coding")
-print (5)
+#take input of a word
+string = input ("Please enter a word: ")
+#take input of a character
+char = input ("Please enter a character: ")
+i = 0
+count = 0
+#loop will to find the occurence of a character
+while (i < len(string)): #string operation
+  if (string[i]== char): #condition 1
+    count = count + 1
+  i = i + 1
+#display the result
+print ("The character", char, "occurs", count, "times in the word", string)
--- activity2.py
+++ activity2.py
@@ -1,6 +1,13 @@
-x = 5
-y = "Driya"
-print (x)
-print (y)
-name = input("Enter your name")
-print ("Hello", name, "Welcome to codingal")
+#take two input from the user
+lower = int(input("Enter an upper range: "))
+upper = int(input("Enter a lower range: "))
+print("Prime numbers between", lower, "and", upper, "are: ")
+#iterate loop from a lower limit to an upper limit
+for num in range (lower, upper + 1):
+#all prime numbers are greater than 1
+ if num > 1:
+    for i in range (2, num):
+        if (num % i) == 0: 
+            break
+    else:
+        print (num)
