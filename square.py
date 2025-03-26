cbf821682243f2495d55f5f0bfc35ca78da9ff38
driyasharmaa <sharmadriya15@gmail.com>
2025-03-26 19:18:44 +0000

    Add files via upload

---
+++ square.py
@@ -0,0 +1,9 @@
+import turtle   #importing library
+square turtle = turtle.Turtle
+square_turtle.speed(5)  #set turtle speed to medium
+#draw a square
+for i in range (4):
+    square_turtle.forward(100)  #move forward 100 units
+    square_turtle.right(90)    #turn right 90 degrees
+#keep the window open until clicked
+turtle.done()
