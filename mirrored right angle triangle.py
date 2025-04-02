138ddcd9d7a660dea21a7c2a4b829e139b412216
driyasharmaa <sharmadriya15@gmail.com>
2025-04-02 19:19:49 +0100

    Add files via upload

--- mirrored right angle triangle.py
+++ mirrored right angle triangle.py
@@ -1,13 +1,7 @@
-#take input
-print("Mirrored right angle triangle pattern of stars (*)")
-n = int(input("Enter the number of rows: "))
-#outer loop to handle number of rows
-for i in range (1, n + 1):
-    #print spaces first
-    for j in range (n - 1):
-        print(" ", end = "")
-    #then print stars
-    for k in range (i):
-        print ("*", end = "")
-    #new line after each row
-    print()
+def mirrored_triangle(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "*" * i)
+    
+# example usage 
+rows = int(input("Enter the number of rows: "))
+mirrored_triangle(rows)
