55b5ff480cf9768f3d7ba2ffcc4d7ad6e8d7a58c
driyasharmaa <sharmadriya15@gmail.com>
2025-06-19 20:20:17 +0100

    Add files via upload

--- activity 1.py
+++ activity 1.py
@@ -1,30 +1,10 @@
-import turtle
+# Import necessary libraries 
+from tkinter import *
 
-turtle.Screen().bgcolor("Aqua")
-board = turtle.Turtle()
- 
-# first triangle for star
-board.forward(100) # draw base
- 
-board.left(120)
-board.forward(100)
- 
-board.left(120)
-board.forward(100)
- 
-board.penup()
-board.right(150)
-board.forward(50)
- 
-# second triangle for star
-board.pendown()
-board.right(90)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-turtle.done()
+# Create Window 
+window = Tk() 
+
+# Set the window Title and Geometry 
+window.title('Demo.Window')
+window.geometry('400x300')
+window.mainloop()
